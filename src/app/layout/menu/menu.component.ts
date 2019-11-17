f272148aba80b4865cc0ee8e9b552c1b68cd662c
davidson <davidson@albeom.com.br>
2019-11-17 19:14:49 -0200

    Ajustes de posicionamento do menu flutuante e pequenas correcoes de layout

--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -5,13 +5,13 @@ import { CadastroService } from '../../services/cadastro.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css', './mobile.css', './tablet.css']
 })
 export class MenuComponent implements OnInit {
   @ViewChild('nav', {static: false}) nav: ElementRef;
   // @ViewChild('burger', {static: false}) burger: ElementRef;
   @ViewChild('navlinks', {static: false}) navlinks: ElementRef;
-  
+
   public shownav = false;
 
   constructor(private router: Router, private service: CadastroService) { }
@@ -27,7 +27,7 @@ export class MenuComponent implements OnInit {
   showHideNav() {
     this.shownav = !this.shownav;
     this.navlinks.nativeElement.classList.toggle('nav-active');
-    
+
     console.log("show hide");
   }
 }
